c488a8e4f6765f325168351e89b3a0047f69dce9
Jens Simon <jens.simon80@googlemail.com>
2021-10-01 17:56:50 +0200

    Disable @typescript-eslint/indent for JS files

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ module.exports = {
       rules: {
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/indent': 'off',
       },
     },
   ],
